5c81f25a186aedd9bb4b55b47432425a5595c267
John Belstner <john.belstner@intel.com>
2019-12-07 11:14:44 -0700

    Removed sensor software repository.

--- docker/build.sh
+++ docker/build.sh
@@ -16,16 +16,16 @@ echo "This script will download and install the Intel® RSP SW Toolkit-"
 echo "Gateway dockerized Java application along with its dependencies."
 echo "This script is designed to run on Debian 9 or Ubuntu 18.04 LTS."
 echo 
-echo "This script will also download and install the latest software"
-echo "repository for Intel® RFID Sensor Platforms (H1000/H3000/H4000)."
-echo "By continuing with this installation, you agree to the terms in"
-echo "the End User License Agreemnt Intel-RSP-EULA-Agreement.pdf."
-echo "https://github.com/intel/rsp-sw-toolkit-installer/sensor-sw-repo"
-echo
-echo "IMPORTANT! PLEASE READ AND AGREE TO THIS EULA BEFORE CONTINUING!"
-echo 
-read -p 'Press [Enter] to continue... or [Ctrl]-[C] to exit...'
-echo
+#echo "This script will also download and install the latest software"
+#echo "repository for Intel® RFID Sensor Platforms (H1000/H3000/H4000)."
+#echo "By continuing with this installation, you agree to the terms in"
+#echo "the End User License Agreemnt Intel-RSP-EULA-Agreement.pdf."
+#echo "https://github.com/intel/rsp-sw-toolkit-installer/sensor-sw-repo"
+#echo
+#echo "IMPORTANT! PLEASE READ AND AGREE TO THIS EULA BEFORE CONTINUING!"
+#echo 
+#read -p 'Press [Enter] to continue... or [Ctrl]-[C] to exit...'
+#echo
 CURRENT_DIR=$(pwd)
 
 echo "Checking Internet connectivity"
